05230cce967e6117e0a37f8070f7d6e67656128a
F. de Almeida Pereira <f.de.almeida.pereira@avanade.com>
2018-06-09 18:32:04 -0300

    varias coisas

--- src/app/modalidade-list.component/modalidade-list.component.ts
+++ src/app/modalidade-list.component/modalidade-list.component.ts
@@ -5,6 +5,7 @@ import { BaseComponent } from "../shared/base-component";
 import { AcademiaService } from "../services/academia.service";
 import { SAMService } from "../services/sam.service";
 import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-gc-modalidade-list',
@@ -12,20 +13,23 @@ import { Router } from "@angular/router";
 })
 
 export class ModalidadeListComponent
-  // extends BaseComponent
+  extends BaseComponent
   implements OnInit {
-
 
   lstModalidade: Modalidade[];
   targetNewModalidade: Modalidade;
   targetAlteraModalidade: Modalidade;
+  _router: Router;
 
   constructor(private oAcademiaService: AcademiaService,
     oSAMService: SAMService,
-    private router: Router,
-    private oModalidadeService: ModalidadeService) {
+    router: Router,
+    private oModalidadeService: ModalidadeService,
+    private toastr: ToastrService) {
 
-    // super(true, oSAMService, router);
+    super(true, oSAMService, router);
+
+    this._router = router;
   }
 
   ngOnInit(): void {
@@ -36,16 +40,23 @@ export class ModalidadeListComponent
   }
 
   Return(sKey: string): void {
-    if (sKey === 'NovaModalidade') {
-      this.targetNewModalidade = undefined;
-      return;
-    }
-    if (sKey === 'AlteraModalidade') {
-      this.targetAlteraModalidade = undefined;
-      return;
-    }
+    this.oModalidadeService.Obtem(this.oAcademiaService.oAcademia)
+      .subscribe((lstModalidade: Modalidade[]) => {
+        this.lstModalidade = lstModalidade;
 
-    this.router.navigate(['/dashboard']);
+        if (sKey === 'NovaModalidade') {
+          this.targetNewModalidade = undefined;
+          return;
+        }
+        if (sKey === 'AlteraModalidade') {
+          this.targetAlteraModalidade = undefined;
+          return;
+        }
+        this._router.navigate(['/dashboard']);
+
+      });
+
+
   }
 
   NovaModalidade(): void {
@@ -55,12 +66,20 @@ export class ModalidadeListComponent
 
 
   ApplyNovaModalidade(): void {
+    if (!this.targetNewModalidade.Nome) {
+      this.toastr.error('Por favor, Digite o nome', 'Nome');
+      return;
+    }
+
+
     this.oModalidadeService.Adiciona(this.targetNewModalidade)
       .subscribe(() => {
         this.oModalidadeService.Obtem(this.oAcademiaService.oAcademia)
           .subscribe((lstModalidade: Modalidade[]) => {
             this.lstModalidade = lstModalidade;
             this.targetNewModalidade = undefined;
+            this.toastr.success('Modalidade adicionada com sucesso', 'Modalidade');
+
           });
       });
   }
@@ -69,13 +88,32 @@ export class ModalidadeListComponent
     this.targetAlteraModalidade = oModalidade;
   }
 
-  ApplyAlteraModalidade(oModalidade: Modalidade): void {
-    this.oModalidadeService.Altera(this.targetNewModalidade)
+  ApplyAlteraModalidade(): void {
+    if (!this.targetAlteraModalidade.Nome) {
+      this.toastr.error('Por favor, Digite o nome', 'Nome');
+      return;
+    }
+
+    this.oModalidadeService.Altera(this.targetAlteraModalidade)
       .subscribe(() => {
         this.oModalidadeService.Obtem(this.oAcademiaService.oAcademia)
           .subscribe((lstModalidade: Modalidade[]) => {
             this.lstModalidade = lstModalidade;
-            this.targetNewModalidade = undefined;
+            this.targetAlteraModalidade = undefined;
+            this.toastr.success('Modalidade alterada com sucesso', 'Modalidade');
+
+          });
+      });
+  }
+
+  ApagarModalidade(oModalidade: Modalidade): void {
+    this.oModalidadeService.Apagar(oModalidade)
+      .subscribe(() => {
+        this.toastr.success('Modalidade apagada com sucesso', 'Modalidade');
+
+        this.oModalidadeService.Obtem(this.oAcademiaService.oAcademia)
+          .subscribe((lstModalidade: Modalidade[]) => {
+            this.lstModalidade = lstModalidade;
           });
       });
   }
--- src/app/services/modalidade.service.ts
+++ src/app/services/modalidade.service.ts
@@ -3,20 +3,32 @@ import { Observable } from "rxjs/Observable";
 import { HttpClient } from "@angular/common/http";
 import { Modalidade } from "../models/modalidade";
 import { Academia } from "../models/academia";
+import { AcademiaService } from "./academia.service";
 
 @Injectable()
 export class ModalidadeService {
-  constructor(private oHttpClient: HttpClient) { }
+  constructor(private oHttpClient: HttpClient, private oAcademia: AcademiaService) { }
 
-  public Obtem(oAcademia: Academia): Observable<any> {
-    return this.oHttpClient.post('ObtemModalidade', oAcademia);
+  public Obtem(oAcademia: Academia, loadActive?: boolean): Observable<any> {
+    return this.oHttpClient.post('ObtemModalidade', oAcademia)
+      .map((values: any[]) => {
+        return values.filter((value) => {
+          return value.IsActive || loadActive;
+        });
+      });
   }
 
   public Altera(oModalidade: Modalidade): Observable<any> {
-    return this.oHttpClient.put('GC_Modalidade/' + oModalidade.Id, oModalidade);
+    return this.oHttpClient.post('AlteraModalidade', oModalidade);
+  }
+
+  public Apagar(oModalidade: Modalidade): Observable<any> {
+    return this.oHttpClient.post('DeletaModalidade', oModalidade);
   }
 
   public Adiciona(oModalidade: Modalidade): Observable<any> {
+    oModalidade['GC_AcademiaId'] = this.oAcademia.oAcademia.Id;
+
     return this.oHttpClient.post('AdicionaModalidade', oModalidade);
   }
 
